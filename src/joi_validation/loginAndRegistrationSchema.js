4ccb547e89a215a46d440b52495604aab34f466e
akilinjamam <akilinjamam@gmail.com>
2024-05-02 13:58:43 -0500

    file-updated

--- src/joi_validation/loginAndRegistrationSchema.js
+++ src/joi_validation/loginAndRegistrationSchema.js
@@ -2,11 +2,11 @@ const joi = require('joi')
 module.exports.registrationSchema = joi.object({
     username: joi.string().min(3).max(31).required(),
     email: joi.string().email().required(),
-    password: joi.string().min(3).max(6).required(),
+    password: joi.string().min(6).max(12).required(),
 })
 
 module.exports.loginSchema = joi.object({
     username: joi.string().min(3).max(31).required(),
-    password: joi.string().min(3).max(6).required()
+    password: joi.string().required()
 })
 
