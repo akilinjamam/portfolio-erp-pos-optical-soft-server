1e4298cb7a04d9d90ccadcaf751f42ceea4c5fb9
akilinjamam <akilinjamam@gmail.com>
2024-09-25 21:56:16 +0600

    registration-page-updated

--- src/modules/registration/registration.model.js
+++ src/modules/registration/registration.model.js
@@ -14,7 +14,59 @@ const registrationSchema = mongoose.Schema({
     password: {
         type: String,
         required: true
-    }
+    },
+    role: {
+        type: String,
+        default: 'user'
+    },
+    accept_by_admin: {
+        type: Boolean,
+        default: false
+    },
+    pos: {
+        type: Boolean,
+        default: false
+    },
+    sales_record: {
+        type: Boolean,
+        default: false
+    },
+    stock: {
+        type: Boolean,
+        default: false
+    },
+    sales_invoice: {
+        type: Boolean,
+        default: false
+    },
+    controll_user_access: {
+        type: Boolean,
+        default: false
+    },
+    employee_list: {
+        type: Boolean,
+        default: false
+    },
+    payroll: {
+        type: Boolean,
+        default: false
+    },
+    user_list: {
+        type: Boolean,
+        default: false
+    },
+    product_entry: {
+        type: Boolean,
+        default: false
+    },
+    product_list: {
+        type: Boolean,
+        default: false
+    },
+    customer_list: {
+        type: Boolean,
+        default: false
+    },
 },
     {
         timestamps: true,
