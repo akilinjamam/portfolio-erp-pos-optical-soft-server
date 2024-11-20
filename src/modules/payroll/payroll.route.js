d1ece7dbb4e241c5983dde7fbc8a710aa5f55fe0
akilinjamam <akilinjamam@gmail.com>
2024-11-20 06:29:06 -0500

    file-updated

--- src/modules/payroll/payroll.route.js
+++ src/modules/payroll/payroll.route.js
@@ -9,7 +9,7 @@ const payrollRouter = require('express').Router();
 
 payrollRouter.post('/create-payroll', runValidator(payrollValidationSchema), createPayrollController);
 
-payrollRouter.get('/', getPayrollController)
+payrollRouter.get('/', passport.authenticate('jwt', { session: false }), getPayrollController)
 payrollRouter.post('/bulk-delete', deletePayrollController)
 payrollRouter.get('/:id', passport.authenticate('jwt', { session: false }), getSinglePayrollController)
 payrollRouter.patch('/:id', updatePayrollController)
