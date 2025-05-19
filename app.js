63cd9f73de923819d2e13626949f46538b475c9f
akilinjamam <akilinjamam@gmail.com>
2025-05-19 13:54:39 +0600

    app.js-updated

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(passport.initialize());
 
 app.use(
     cors({
-        origin: [`${process.env.CLIENT_URL_LINK}`],
+        origin: [`${process.env.CLIENT_URL_LINK}`, 'http://localhost:5173'],
         methods: 'GET,POST,PUT,DELETE,PATCH',
         credentials: true,
     }),
