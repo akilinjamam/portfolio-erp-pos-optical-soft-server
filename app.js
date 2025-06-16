de61f64f1497bda37b6211ee5a7974468cf2abf8
akilinjamam <akilinjamam@gmail.com>
2025-06-16 07:05:34 +0600

    cors-updated

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: ['http://localhost:5173'],
+        origin: ['https://opticalsoft-client.vercel.app', 'http://localhost:5173'],
         methods: ['GET', 'POST'],
         credentials: true
     }
